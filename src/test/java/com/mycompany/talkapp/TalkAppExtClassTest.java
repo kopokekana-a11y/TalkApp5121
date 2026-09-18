687df46ce6e13d1a02e030854f0ff6e454f80353
Student <Student@LabVM2049939.bz31bhys5miujfwirum1cxq1uc.jnbx.internal.cloudapp.net>
2026-09-18 10:53:08 +0200

    added third test and it was successful

--- src/test/java/com/mycompany/talkapp/TalkAppExtClassTest.java
+++ src/test/java/com/mycompany/talkapp/TalkAppExtClassTest.java
@@ -11,12 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Student
  */
-public class TalkAppExtClassTest {
-    @Test
-    public void testCheckUserName() { 
-        TalkAppExtClass user =new TalkAppExtClass("kyl_1", "Ch&&set@ke99!", "+27838968976");
-}       assertTrue(user.checkUserName()); } 
+    public class TalkAppExtClassTest {
+        @Test
+        public void testCheckUserName() { 
+            TalkAppExtClass user =new TalkAppExtClass("kyl_1", "Ch&&set@ke99!", "+27838968976");
+    }       assertTrue(user.checkUserName()); } 
 
-@Test public void testInvalidUserName() { 
-    TalkAppExtClass user = new TalkAppExtClass("kyle !!!!!!!", "Ch&&set@ke99!", "+27838968976"); 
-    assertFalse(user.checkUserName()); }
+        @Test
+        public void testInvalidUserName() { 
+        TalkAppExtClass user = new TalkAppExtClass("kyle !!!!!!!", "Ch&&set@ke99!", "+27838968976"); 
+        assertFalse(user.checkUserName()); }
+        
+        @Test 
+        public void testCheckPasswordComplexity() { 
+            TalkAppExtClass user = new TalkAppExtClass("kyl_1", "Ch&&set@ke99!", "+27838968976"); 
+            assertTrue(user.checkPasswordComplexity()); }
